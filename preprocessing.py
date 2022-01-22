97519544aef80c56cab7633e523c50590c75dd0a
Kenneth Shang <Kenneth.Shang@foxmail.com>
2022-01-23 00:58:06 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -15,7 +15,8 @@ from Bio.SeqRecord import SeqRecord
 ########################  Parameters  #######################
 #############################################################
 
-parser = argparse.ArgumentParser(description='manual to this script')
+parser = argparse.ArgumentParser(description="""PhaMer is a python library for identifying bacteriophages from metagenomic data. 
+                                 PhaMer is based on a Transorfer model and rely on protein-based vocabulary to convert DNA sequences into sentences.""")
 parser.add_argument('--contigs', help='FASTA file of contigs',  default = 'test_contigs.fa')
 parser.add_argument('--len', help='minimun length of contigs', type=int, default=3000)
 inputs = parser.parse_args()
