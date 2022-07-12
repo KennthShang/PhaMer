4d4c54343394431b61d5cc06361cbc3791894b89
Sebastian Jaenicke <sebastian.jaenicke@computational.bio.uni-giessen.de>
2022-07-12 13:07:11 +0200

    allow to specify different database directory; silence external processes

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,7 @@ import pickle as pkl
 import subprocess
 import argparse
 import shutil
+from shutil import which
 from collections import Counter
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
@@ -22,6 +23,7 @@ parser.add_argument('--contigs', help='FASTA file of contigs',  default = 'test_
 parser.add_argument('--proteins', help='FASTA file of predicted proteins (optional)')
 parser.add_argument('--len', help='minimum length of contigs', type=int, default=3000)
 parser.add_argument('--threads', help='number of threads to use', type=int, default=8)
+parser.add_argument('--dbdir', help='database directory (optional)',  default = 'database')
 parser.add_argument('--midfolder', help='folder to store the intermediate files', type=str, default='phamer/')
 inputs = parser.parse_args()
 
@@ -35,6 +37,11 @@ transformer_fn = inputs.midfolder
 
 if not os.path.isdir(out_fn):
     os.makedirs(out_fn)
+
+db_dir = inputs.dbdir
+if not os.path.exists(db_dir):
+    print(f'Database directory {db_dir} missing or unreadable')
+    exit(1)
 
 #############################################################
 ##################  Filter short contigs  ###################
@@ -55,38 +62,54 @@ if inputs.proteins is None:
     prodigal = "prodigal"
     # check if pprodigal is available
     if which("pprodigal") is not None:
+        print("Using parallelized prodigal...")
         prodigal = f'pprodigal -T {threads}'
 
     prodigal_cmd = f'{prodigal} -i {out_fn}/filtered_contigs.fa -a {out_fn}/test_protein.fa -f gff -p meta'
     print("Running prodigal...")
-    _ = subprocess.check_call(prodigal_cmd, shell=True)
+    _ = subprocess.check_call(prodigal_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 else:
     shutil.copyfile(inputs.proteins, f'{out_fn}/test_protein.fa')
 
 
 
 #############################################################
-####################  DIAMOND BLASTP  #######################
+####################  DIAMOND CREATEDB ######################
 #############################################################
 
 
 print("\n\n" + "{:-^80}".format("Diamond BLASTp"))
 print("Creating Diamond database and running Diamond...")
 
+diamond_db = f'{db_dir}/database.dmnd'
+
 try:
-    # create database
-    make_diamond_cmd = f'diamond makedb --threads {threads} --in database/database.fa -d {out_fn}/database.dmnd'
-    print("Creating Diamond database...")
-    _ = subprocess.check_call(make_diamond_cmd, shell=True)
+    if os.path.exists(diamond_db):
+        print(f'Using preformatted DIAMOND database ({diamond_db}) ...')
+    else:
+        # create database
+        make_diamond_cmd = f'diamond makedb --threads {threads} --in {db_dir}/database.fa -d {out_fn}/database.dmnd'
+        print("Creating Diamond database...")
+        _ = subprocess.check_call(make_diamond_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        diamond_db = f'{out_fn}/database.dmnd'
+except:
+    print("diamond makedb failed")
+    exit(1)
+
+#############################################################
+####################  DIAMOND BLASTP  #######################
+#############################################################
+
+try:
     # running alignment
-    diamond_cmd = f'diamond blastp --threads {threads} --sensitive -d {out_fn}/database.dmnd -q {out_fn}/test_protein.fa -o {out_fn}/results.tab -k 1'
+    diamond_cmd = f'diamond blastp --threads {threads} --sensitive -d {diamond_db} -q {out_fn}/test_protein.fa -o {out_fn}/results.tab -k 1'
     print("Running Diamond...")
-    _ = subprocess.check_call(diamond_cmd, shell=True)
+    _ = subprocess.check_call(diamond_cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     diamond_out_fp = f"{out_fn}/results.tab"
     database_abc_fp = f"{out_fn}/results.abc"
     _ = subprocess.check_call("awk '$1!=$2 {{print $1,$2,$11}}' {0} > {1}".format(diamond_out_fp, database_abc_fp), shell=True)
 except:
-    print("create database failed")
+    print("diamond blastp failed")
     exit(1)
 
 
@@ -98,7 +121,7 @@ except:
 
 
 # Load dictonary and BLAST results
-proteins_df = pd.read_csv('database/proteins.csv')
+proteins_df = pd.read_csv(f'{db_dir}/proteins.csv')
 proteins_df.dropna(axis=0, how='any', inplace=True)
 pc2wordsid = {pc: idx for idx, pc in enumerate(sorted(set(proteins_df['cluster'].values)))}
 protein2pc = {protein: pc for protein, pc in zip(proteins_df['protein_id'].values, proteins_df['cluster'].values)}
